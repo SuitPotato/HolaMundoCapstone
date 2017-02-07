84b849060466c71704c736a4a6ea579ae03ec885
スーツ芋 <Erekyth@yahoo.com>
2017-02-07 15:54:05 -0700

    forms.py
    ModelForm (UserForm). Used to create users

--- HolaMundoSite/mainpage/forms.py
+++ HolaMundoSite/mainpage/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+
+# A ModelForm automatically builds your form off a model you provide. It handles the 
+# validations based on the fields.
+# The model User, does not need to be defined in the models.py because it is built in
+# to Django.
+class UserForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
