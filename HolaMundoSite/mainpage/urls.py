465bb7c1a330e53d8c0c3f3a3cab11d6303c5b40
スーツ芋 <Erekyth@yahoo.com>
2017-02-11 17:09:53 -0700

    urls.py additions

--- HolaMundoSite/mainpage/urls.py
+++ HolaMundoSite/mainpage/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -22,4 +23,5 @@ urlpatterns = [
 	url(r'^results/$', views.results),
 	url(r'^loginview/$', views.loginview),
 	url(r'^adduser', views.lexusadduser)
+	url(r'^login', views.login)
 ]
