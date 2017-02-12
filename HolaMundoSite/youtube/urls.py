106bd14d3b663b63b301eff143fe8951bcccad4d
Joshua Sommers <Joshuadsommers@gmail.com>
2017-02-11 18:22:45 -0700

    Initial setup of youtube app
    Added urls.py, views.py, installed app in HolaMundoSite/settings, added
    link to urls.py

--- HolaMundoSite/youtube/urls.py
+++ HolaMundoSite/youtube/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('mainpage.urls')),
-    url(r'^', include('Video_page.urls')),
-    url(r'^', include('coursemanagement.urls')),
-    url(r'^', include('youtube.urls'))
+    url(r'^youtube', views.index),
 ]
