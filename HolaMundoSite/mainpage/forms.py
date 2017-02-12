cd96793f2e3211112f50c298c81a9050f738ab4e
スーツ芋 <Erekyth@yahoo.com>
2017-02-11 17:10:57 -0700

    Forms.py Readability Changes
    Added an empty line to separate from the comments

--- HolaMundoSite/mainpage/forms.py
+++ HolaMundoSite/mainpage/forms.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 # validations based on the fields.
 # The model User, does not need to be defined in the models.py because it is built in
 # to Django.
+
 class UserForm(ModelForm):
 	class Meta:
 		model = User
