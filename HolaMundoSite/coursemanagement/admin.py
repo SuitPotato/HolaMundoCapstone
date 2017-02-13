1997246010a0e0a78f78519243e73864cdf66d79
スーツ芋 <Erekyth@yahoo.com>
2017-02-12 17:51:54 -0700

    Admin.py Course

--- HolaMundoSite/coursemanagement/admin.py
+++ HolaMundoSite/coursemanagement/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from coursemanagement.models import Lesson
+from coursemanagement.models import Lesson, Course
 
 # Register your models here.
 admin.site.register(Lesson)
+admin.site.register(Course)
