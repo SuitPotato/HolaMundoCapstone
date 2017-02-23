d640ea8557c2f8e9bf5aff4b73c6fa8df694d6d6
Nigel-Sultan <ns672@nau.edu>
2017-02-23 15:06:27 -0700

    Added matching to installed apps

--- HolaMundoSite/HolaMundoSite/settings.py
+++ HolaMundoSite/HolaMundoSite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'Video_page',
     'coursemanagement',
     'youtube',
+	'matching',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
