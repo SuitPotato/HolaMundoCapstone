ad3ef47c88796b5f3371ae02e87111b2daf8280d
Andy Salazar <as2653@nau.edu>
2017-02-28 17:16:55 -0700

    fixed urls.py

--- HolaMundoSite/fillintheblank/admin.py
+++ HolaMundoSite/fillintheblank/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from fillintheblank.models import Course, Text, Quiz, Question, Answer
 
 # Register your models here.
-admin.site.register(Course)
-admin.site.register(Text)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Answer)
--- HolaMundoSite/fillintheblank/urls.py
+++ HolaMundoSite/fillintheblank/urls.py
@@ -1,7 +1,26 @@
-from django.conf.urls import url
 
+"""
+HolaMundoSite URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
 from . import views
+from django.contrib.auth.views import login, logout
+
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^fb_quiz/$', views.fb_quiz, name='fb_quiz'),
+    url(r'^FillInTheBlank',views.fb_quiz)
 ]
