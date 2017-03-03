5a2cd1fde219f37c6b7514b3d9f505a8958a9902
Joshua Sommers <Joshuadsommers@gmail.com>
2017-03-02 17:15:29 -0700

    Basic preferences choices
    Added Languages/difficulties preferences

--- HolaMundoSite/UserSettingsPage/models.py
+++ HolaMundoSite/UserSettingsPage/models.py
@@ -1,7 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Preference(models.Model):
+
+    # Autoincrement ID and user
+    prefID = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, null=False, blank=False)
+
+    # Listed choices for preferences
+    LANGUAGES = (
+        ('English', '1'),
+        ('Spanish', '2'),
+    )
+
+    DIFFICULTIES = (
+        ('Beginner', '1'),
+        ('Intermediate', '2'),
+        ('Advanced', '3'),
+    )
+
+    # Setting preferences
+    defaultLanguage = models.CharField(max_length=10, choices=LANGUAGES, default="1")
+    difficulty = models.CharField(max_length=20, choices=DIFFICULTIES, default="1")
+
+    def __str__(self):
+        return self.user
