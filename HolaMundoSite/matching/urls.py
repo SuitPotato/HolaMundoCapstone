99ccad7dfbf1a39a28f5fc0f4423b97b5305ba2f
Nigel-Sultan <ns672@nau.edu>
2017-03-06 13:11:36 -0700

    Added Answer Question view to URLs file

--- HolaMundoSite/matching/urls.py
+++ HolaMundoSite/matching/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^get_number', views.get_number),
 	url(r'^get_questions', views.get_questions),
-	url(r'^answer_question', views.get_questions),
+	url(r'^answer_question', views.answer_question),
 ]
