8873fa3a284e40ca6be8ae35ce578079c459e509
Andy Salazar <as2653@nau.edu>
2017-03-05 18:46:55 -0700

    added urls

--- HolaMundoSite/fillintheblank/urls.py
+++ HolaMundoSite/fillintheblank/urls.py
@@ -21,6 +21,9 @@ from django.contrib.auth.views import login, logout
 
 
 urlpatterns = [
+	url(r'^$', views.index, name ='fillintheblank'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 	#url(r'^shortanswer/' incl)
     #url(r'^fb_quiz/', views.fb_quiz, name='fb_quiz'),
     #url(r'^FillInTheBlank',views.fb_quiz)
