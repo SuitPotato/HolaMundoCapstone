f9071c359d9501411d38d602157f16a55165ded3
ashleyg28 <ang97@nau.edu>
2017-03-07 12:30:14 -0700

    add login required functionality to form

--- HolaMundoSite/multipleChoice/views.py
+++ HolaMundoSite/multipleChoice/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.forms import ModelForm
+from django.db import models
+from multipleChoice.forms import (
+    QuestionForm
+)
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def quiz(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.question, request.POST)
+        if form.is_valid():
+            question = form.save()
+            return redirect('www.djangoproject.com')
+        else:
+            return redirect('mainpage/DragDemo')
+    else:
+        form = QuestionForm(request.question)
+        return render(request, 'multipleChoice/quiz.html', {'form': form})
     return render(request, 'multipleChoice/quiz.html', {})
