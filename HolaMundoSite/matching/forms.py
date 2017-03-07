b4eacc7f483be0d984661c935fc80fe92d0c9b99
Nigel-Sultan <ns672@nau.edu>
2017-03-07 09:57:06 -0700

    Delete True or False

--- HolaMundoSite/matching/forms.py
+++ HolaMundoSite/matching/forms.py
@@ -1,12 +1,29 @@
 from django import forms
+from django.forms import ModelForm
+from matching.models import *
 
-class MatchingNumber(forms.Form):
-	number = forms.IntegerField(label='How many matching questions do you want?', max_value=26, min_value=2)
+class MatchingNumber(ModelForm):
+	class Meta:
+		model = Number
+		fields = ['quiz_name', 'number',]
+	#quiz_name = forms.CharField(label='Quiz Name')
+	#number = forms.IntegerField(label='How many matching questions do you want?', max_value=26, min_value=2)
 
-class MatchingQuestion(forms.Form):
-	question = forms.CharField(label='Question:')
-	answer = forms.CharField(label='Answer')
-	rand_letter = forms.CharField(label='Letter')
+class MatchingQuestion(ModelForm):
+	class Meta:
+		model = Question
+		fields = ['question', 'answer', 'rand_letter',]
+	'''MN = MatchingNumber(forms.ModelForm)
+	copynumber = MN.number
+	temp = 0
+	while(temp != copynumber):
+		question = forms.CharField(label='Question:')
+		answer = forms.CharField(label='Answer')
+		rand_letter = forms.CharField(label='Letter')
+		temp += 1'''
 	
-class MatchingAnswer(forms.Form):
-	user_answer = forms.CharField(label='Your answer:')
+class MatchingAnswer(ModelForm):
+	class Meta:
+		model = Answer
+		fields = ['answer',]
+	#user_answer = forms.CharField(label='Your answer:')
--- HolaMundoSite/matching/models.py
+++ HolaMundoSite/matching/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 # Create your models here.
 
-class MatchingNumber(models.Model):
+class Number(models.Model):
+	quiz_name = models.CharField(max_length=50)
 	number = models.IntegerField()
 	def __str__(self):
-		return self.number
+		return self.quiz_name
 	
-class MatchingQuestion(models.Model):
+class Question(models.Model):
 	question = models.CharField(max_length=250)
 	answer = models.CharField(max_length=250)
 	rand_letter = models.CharField(max_length=1)
