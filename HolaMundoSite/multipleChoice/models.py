5fd624964ede75f37bb1acc724ae5ebd8e97624d
ashleyg28 <ang97@nau.edu>
2017-03-06 23:30:34 -0700

    remove unecessary app

--- HolaMundoSite/multipleChoice/models.py
+++ HolaMundoSite/multipleChoice/models.py
@@ -6,14 +6,8 @@ from django.db import models
 class Questions(models.Model):
     question = models.CharField(max_length=150)
 
-    class Meta:
-        db_table = "MultipleChoiceQuiz"
-
 class Answers(models.Model):
     answer_a = models.CharField(max_length=150)
     answer_b = models.CharField(max_length=150)
     answer_c = models.CharField(max_length=150)
     answer_d = models.CharField(max_length=150)
-
-    class Meta:
-        db_table = "MultipleChoiceQuiz"
