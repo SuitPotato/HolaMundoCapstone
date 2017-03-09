fd856be306e89c5a6708c240e34c4b8e11910eca
ashleyg28 <ang97@nau.edu>
2017-03-09 10:12:05 -0700

    fix error in table

--- HolaMundoSite/multipleChoice/models.py
+++ HolaMundoSite/multipleChoice/models.py
@@ -2,16 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
 
 class Question(models.Model):
+    # id = models.AutoField(primary_key=True)
     question = models.CharField(max_length=150)
     answer_a = models.CharField(max_length=150, default='')
     answer_b = models.CharField(max_length=150, default='')
     answer_c = models.CharField(max_length=150, default='')
     answer_d = models.CharField(max_length=150, default='')
 
-
-
+    #
+    #
     class Meta:
         db_table = "multipleChoice"
 
--- HolaMundoSite/multipleChoice/views.py
+++ HolaMundoSite/multipleChoice/views.py
@@ -10,14 +10,14 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def quiz(request):
-    if request.method == 'POST':
-        form = QuestionForm(request.question, request.POST)
-        if form.is_valid():
-            question = form.save()
-            return redirect('www.djangoproject.com')
-        else:
-            return redirect('mainpage/DragDemo')
-    else:
-        form = QuestionForm(request.question)
-        return render(request, 'multipleChoice/quiz.html', {'form': form})
+    # if request.method == 'POST':
+    #     form = QuestionForm(request.user, request.POST)
+    #     if form.is_valid():
+    #         question = form.save()
+    #         return redirect('www.djangoproject.com')
+    #     else:
+    #         return redirect('mainpage/DragDemo')
+    # else:
+    #     form = QuestionForm(request.user)
+    #     return render(request, 'multipleChoice/quiz.html', {'form': form})
     return render(request, 'multipleChoice/quiz.html', {})
