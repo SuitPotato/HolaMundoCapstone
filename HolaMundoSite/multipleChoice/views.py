051798ef5037cd431b4d6cb47dcfe90f403f8c0b
ashleyg28 <ang97@nau.edu>
2017-03-15 09:51:46 -0700

    able to create form

--- HolaMundoSite/multipleChoice/views.py
+++ HolaMundoSite/multipleChoice/views.py
@@ -11,17 +11,5 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def quiz(request):
-    # if request.method == 'POST':
-    #     form = QuestionForm(request.POST)
-    #     if form.is_valid():
-    #         question = form.save()
-    #         return HttpResponseRedirect('https://www.djangoproject.com/')
-    #     else:
-    #         return HttpResponseRedirect('https://www.djangoproject.com/')
-    # else:
-    #     return HttpResponseRedirect('https://www.djangoproject.com/')
-    #     form = QuestionForm(request.user)
-    #     return render(request, 'multipleChoice/quiz.html', {'form': form})
-    # return render(request, 'multipleChoice/quiz.html', {})
     form = QuestionForm()
     return render(request, 'multipleChoice/quiz.html', {'form': form})
