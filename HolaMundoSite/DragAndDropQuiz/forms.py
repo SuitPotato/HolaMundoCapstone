90c7c4426a9d457406e2e2272f8b499e4d4fdf8d
スーツ芋 <Erekyth@yahoo.com>
2017-03-20 21:08:06 -0700

    Form Fields

--- HolaMundoSite/DragAndDropQuiz/forms.py
+++ HolaMundoSite/DragAndDropQuiz/forms.py
@@ -12,6 +12,15 @@ class DragAndDropForm(forms.Form):
 	class Meta:
 		# Based off of the models
 		model = DragAndDrop
-		fields = []
+		fields = ['title','content','wordOne','wordTwo','wordThree','wordFour','wordFive']
 		
 	# Fields defined under forms.____
+	title = forms.CharField(max_length = 140)
+	content = forms.CharField(max_length = 1000)
+	
+	# Extend later to accept more words
+	wordOne = forms.CharField(max_length = 15)
+	wordTwo = forms.CharField(max_length = 15)
+	wordThree = forms.CharField(max_length = 15)
+	wordFour = forms.CharField(max_length = 15)
+	wordFive = forms.CharField(max_length = 15)
