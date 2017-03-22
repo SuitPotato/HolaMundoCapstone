1d9e73999ce1f7e361be9de19d395c0b6de209a1
スーツ芋 <Erekyth@yahoo.com>
2017-03-22 12:17:38 -0700

    Views for the new drag n drop method

--- HolaMundoSite/DragAndDropQuiz/views.py
+++ HolaMundoSite/DragAndDropQuiz/views.py
@@ -7,3 +7,12 @@ def view_dragndrop(request):
 
 def create_dragndrop(request):
     return None
+	
+	
+# The following views are using the new method of Drag and drop
+
+def view_sentence_drag(request):
+	return None
+	
+def create_sentence_drag(request):
+	return None
