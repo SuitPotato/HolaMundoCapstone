5c99a09d632432873e2a364fc8243df4005e968e
Andy Salazar <as2653@nau.edu>
2017-03-22 20:43:55 -0700

    added comments

--- HolaMundoSite/fillintheblank/models.py
+++ HolaMundoSite/fillintheblank/models.py
@@ -2,11 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# Required for importing User for Author
 from django.contrib.auth.models import User
 
+# Short Answer/Essay Question Model
 class Question(models.Model):
+	# Text Field for Question
 	question = models.CharField(max_length=250, default = '')
+	# Text Field for answer
 	answer = models.CharField(max_length=500, default = '')
+	# Key for question
 	key = models.IntegerField(default=0)
 
 	class Meta:
@@ -18,29 +23,17 @@ class Answer(models.Model):
 	def __str__(self):
 		return self.answer
 
+# Fill In The Blank Question Model
 class FillInTheBlankQuestion(models.Model):
+	# Beginning of sentence
 	question_start = models.CharField(max_length=50, default = '')
+	# End of sentence
 	question_end = models.CharField(max_length=50, default = '')
+	# Answer for Fill In The Blank
 	answer = models.CharField(max_length=50, default = '')
+	# Key for question
 	key = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.answer
 
-'''from __future__ import unicode_literals
-
-from django.db import models
-
-class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-
-	def __str__(self):
-		return self.question_text
-
-class Answer(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=500)
-
-	def __str__(self):
-		return self.choice_text
-'''
