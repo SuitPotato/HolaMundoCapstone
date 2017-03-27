9154a5462eb5fe3fab3b71c8580595643a81af29
Nigel-Sultan <ns672@nau.edu>
2017-03-27 16:19:44 -0700

    Updated admin file to only include new app

--- HolaMundoSite/matching/admin.py
+++ HolaMundoSite/matching/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from matching.models import *
 
 # Register your models here.
-admin.site.register(Number)
-admin.site.register(Question)
-admin.site.register(Answer)
+#admin.site.register(Number)
+#admin.site.register(Question)
+#admin.site.register(Answer)
 admin.site.register(Matching)
