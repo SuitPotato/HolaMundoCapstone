bc05e4850a2abcde8b872dd727176766d6713e2d
Joshua Sommers <Joshuadsommers@gmail.com>
2017-04-02 22:33:12 -0700

    Created logic for handling myProfile views.py
    Added ways to handle loading/saving of all 3 forms for users in views.py

--- HolaMundoSite/UserSettingsPage/views.py
+++ HolaMundoSite/UserSettingsPage/views.py
@@ -1,4 +1,5 @@
 # Django Imports
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.forms import ModelForm
@@ -13,7 +14,7 @@ from UserSettingsPage.forms import (
 )
 
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm, SetPasswordForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
@@ -28,6 +29,7 @@ def settings(request):
     else:
         return render(request, 'UserSettingsPage/settings.html')
 
+
 @login_required()
 def passwordform(request):
     if request.method == 'POST':
@@ -37,23 +39,73 @@ def passwordform(request):
             update_session_auth_hash(request, user)  # Important!
             return redirect('UserSettingsPage:password')
         else:
-                # if form is not valid go here
-                return redirect('mainpage/DragDemo')
+            # if form is not valid go here
+            return redirect('mainpage/DragDemo')
     else:
         form = PasswordChangeForm(request.user)
         return render(request, 'UserSettingsPage/passwordform.html', {'form': form})
 
+
 @login_required()
 def view_profile(request):
-	try:
-		prefs = Preference.objects.get(user=request.user)
-		args = {'user': request.user, 'prefs': prefs}
-		return render(request, 'UserSettingsPage/profile.html', args)
-	except:
-		prefs = Preference.objects.create(user=request.user)
-		prefs = Preference.objects.get(user=request.user)
-		args = {'user': request.user, 'prefs': prefs}
-		return render(request, 'UserSettingsPage/profile.html', args)
+    try:
+        prefs = Preference.objects.get(user=request.user)
+        args = {'user': request.user, 'prefs': prefs}
+        return render(request, 'UserSettingsPage/profile.html', args)
+    except:
+        prefs = Preference.objects.create(user=request.user)
+        prefs = Preference.objects.get(user=request.user)
+        args = {'user': request.user, 'prefs': prefs}
+        return render(request, 'UserSettingsPage/profile.html', args)
+
+
+@login_required()
+def view_my_profile(request):
+    if request.method == 'POST':
+        print(request.POST)
+        if request.POST.get("savebutton") == "Save User info":
+            print('1')
+            form = EditProfileForm(request.POST, instance=request.user)
+
+            if form.is_valid():
+                form.save()
+                return render(request, 'UserSettingsPage/myProfile.html')
+
+        elif request.POST.get("savebutton") == "Save Password":
+            print('2')
+            form = SetPasswordForm(request.user, request.POST)
+
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                return HttpResponseRedirect('/myProfile/')
+
+            else:
+                return HttpResponseRedirect('/myProfile/')
+
+        elif request.POST.get("savebutton") == "Save User Prefs":
+            prefs = Preference.objects.get(user=request.user)
+            print(prefs.LANGUAGES)
+            print(request.POST['user_language'])
+            print(request.POST['user_difficulty'])
+            prefs.defaultLanguage = prefs.LANGUAGES[int(request.POST['user_language'])-1][0]
+            prefs.difficulty = prefs.DIFFICULTIES[int(request.POST['user_difficulty'])-1][0]
+            prefs.save()
+            return HttpResponseRedirect('/myProfile/')
+
+    else:
+        try:
+            prefs = Preference.objects.get(user=request.user)
+            args = {'user': request.user, 'prefs': prefs}
+            print(prefs.difficulty)
+            print(prefs.defaultLanguage)
+            return render(request, 'UserSettingsPage/myProfile.html', args)
+        except:
+            prefs = Preference.objects.create(user=request.user)
+            prefs = Preference.objects.get(user=request.user)
+            args = {'user': request.user, 'prefs': prefs}
+            return render(request, 'UserSettingsPage/myProfile.html', args)
+
 
 @login_required()
 def edit_profile(request):
@@ -63,7 +115,7 @@ def edit_profile(request):
         if form.is_valid():
             form.save()
             args = {'form': form}
-            return render(request, 'UserSettingsPage/profile.html', args )
+            return render(request, 'UserSettingsPage/profile.html', args)
     else:
         form = EditProfileForm(instance=request.user)
         args = {'form': form}
