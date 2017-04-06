2625b05e09eb08aaf8dda5fd984820f5a2121d55
Joshua Sommers <Joshuadsommers@gmail.com>
2017-04-05 20:39:19 -0700

    Revert "Database update + migrations for MC quiz"
    This reverts commit c043e9fd9c830ccb8207cd1bc6c386e853439b36.

--- HolaMundoSite/coursemanagement/admin.py
+++ HolaMundoSite/coursemanagement/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from coursemanagement.models import Lesson, Course, Quiz, CourseLessonQuiz, MultipleChoiceQuiz, MultipleChoiceQuizResponse
+from coursemanagement.models import Lesson, Course, Quiz, CourseLessonQuiz
 
 # Register your models here.
 admin.site.register(Lesson)
 admin.site.register(Course)
 admin.site.register(Quiz)
 admin.site.register(CourseLessonQuiz)
-admin.site.register(MultipleChoiceQuiz)
-admin.site.register(MultipleChoiceQuizResponse)
