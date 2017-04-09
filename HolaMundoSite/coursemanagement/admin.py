8da45cce38c95123d4e0dc25f1eb9a8cc7cedd96
スーツ芋 <Erekyth@yahoo.com>
2017-04-09 16:27:27 -0700

    Admin.py updates
    1. Separated with comments
    2. Added ShortAnswerQuiz and Response

--- HolaMundoSite/coursemanagement/admin.py
+++ HolaMundoSite/coursemanagement/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from coursemanagement.models import Lesson, Course, Quiz, CourseLessonQuiz, MultipleChoiceQuiz, MultipleChoiceQuizResponse
+from coursemanagement.models import ShortAnswerQuiz, ShortAnswerQuizResponse
 
 # Register your models here.
+# Course Management Models
 admin.site.register(Lesson)
 admin.site.register(Course)
 admin.site.register(Quiz)
 admin.site.register(CourseLessonQuiz)
+
+# Multiple Choice Models
 admin.site.register(MultipleChoiceQuiz)
 admin.site.register(MultipleChoiceQuizResponse)
+
+# Short Answer Quiz models
+admin.site.register(ShortAnswerQuiz)
+admin.site.register(ShortAnswerQuizResponse)
