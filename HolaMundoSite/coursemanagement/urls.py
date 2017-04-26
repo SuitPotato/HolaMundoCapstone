efbe2559e23d3ba0234fa4cc48f1a4db2646829e
Nigel-Sultan <ns672@nau.edu>
2017-04-25 19:55:04 -0700

    coursemanagement permissions

--- HolaMundoSite/coursemanagement/urls.py
+++ HolaMundoSite/coursemanagement/urls.py
@@ -52,5 +52,7 @@ urlpatterns = [
     # url(r'^create', views.create),
     # url(r'^createcourse', views.course),
     # url(r'^createquize', views.quiz),
+	
+	url(r'^denied/$', views.denied, name='denied'),
 
 ]
