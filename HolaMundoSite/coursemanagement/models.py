b3ca8c6b14323f16b94caa757c2eb97247455b63
スーツ芋 <Erekyth@yahoo.com>
2017-04-26 19:07:45 -0700

    Max_Length Change
    Changed temporarily for testing. Will need to update with better
    CharField

--- HolaMundoSite/coursemanagement/models.py
+++ HolaMundoSite/coursemanagement/models.py
@@ -15,7 +15,7 @@ class Lesson(models.Model):
 	lessonID = models.AutoField(primary_key=True)
 	assignedCourse = models.ForeignKey('coursemanagement.Course', on_delete=models.CASCADE,
 									   related_name='assigned_course', null=True, blank=True)
-	title = models.CharField(max_length=55)
+	title = models.CharField(max_length=140)
 	youtube = models.CharField(max_length=100)
 	author = models.ForeignKey(User, null=False, blank=False)
 
@@ -68,7 +68,7 @@ class Quiz(models.Model):
 	# QuizID - References the Primary Key
 	quizID = models.AutoField(primary_key=True)
 	LessonID = models.ForeignKey(Lesson, null=False, blank=False)
-	title = models.CharField(max_length=55)
+	title = models.CharField(max_length=140)
 
 	# Potentially add the content of non-draggable sentence
 	# content = models.CharField(max_length = 1000)
@@ -87,7 +87,7 @@ class Quiz(models.Model):
 
 class Course(models.Model):
 	courseID = models.AutoField(primary_key=True)
-	title = models.CharField(max_length=55)
+	title = models.CharField(max_length=100)
 	author = models.ForeignKey(User, null=False, blank=False)
 	date = models.DateField(auto_now_add=True)
 	link = models.CharField(max_length=15)
@@ -127,7 +127,7 @@ class MultipleChoiceQuiz(models.Model):
 	# QuizID - References the Primary Key
 	quizID = models.AutoField(primary_key=True)
 	LessonID = models.ForeignKey(Lesson, null=True, blank=True)
-	title = models.CharField(max_length=55)
+	title = models.CharField(max_length=140)
 	author = models.ForeignKey(User, null=False, blank=False)
 
 	NUMBER_OF_CHOICES = (
@@ -172,7 +172,7 @@ class MultipleChoiceQuiz(models.Model):
 class ShortAnswerQuiz(models.Model):
 	quizID = models.AutoField(primary_key = True)
 	LessonID = models.ForeignKey(Lesson, null = True, blank = False)
-	title = models.CharField(max_length=55, null=False, blank=False)
+	title = models.CharField(max_length=100, null=False, blank=False)
 	author = models.ForeignKey(User, null = False, blank = False)
 	
 	DIFFICULTIES = (
@@ -242,3 +242,77 @@ class MultipleChoiceQuizResponse(models.Model):
 
 	def __str__(self):
 		return self.quizID.title + ": " + self.user.username
+
+class MatchingQuiz(models.Model):
+	quizID = models.AutoField(primary_key = True)
+	LessonID = models.ForeignKey(Lesson, null = True, blank = True)
+	title = models.CharField(max_length = 140)
+	author = models.ForeignKey(User, null = False, blank = False)
+	
+	NUMBER_OF_OPTIONS = {
+		{'2',2},
+		{'3',3},
+		{'4',4},
+		{'5',5},
+		{'6',6},
+		{'7',7},
+		{'8',8},
+		{'9',9},
+		{'10',10},
+		{'11',11},
+		{'12',12},
+		{'13',13},
+		{'14',14},
+		{'15',15},
+	}
+	
+	numberOfOptions = models.CharField(max_length=2, choices =)
+	
+	matchOne = models.CharField(max_length = 25, null = False, blank = False)
+	answerOne = models.CharField(max_length = 25, null = False, blank = False)
+	
+	matchTwo = models.CharField(max_length = 25, null = False, blank = False)
+	answerTwo = models.CharField(max_length = 25, null = False, blank = False)
+	
+	matchThree = models.CharField(max_length = 25, null = True, blank = True)
+	answerThree = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchFour = models.CharField(max_length = 25, null = True, blank = True)
+	answerFour = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchFive = models.CharField(max_length = 25, null = True, blank = True)
+	answerFive = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchSix = models.CharField(max_length = 25, null = True, blank = True)
+	answerSix = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchSeven = models.CharField(max_length = 25, null = True, blank = True)
+	answerSeven = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchEight = models.CharField(max_length = 25, null = True, blank = True)
+	answerEight = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchNine = models.CharField(max_length = 25, null = True, blank = True)
+	answerNine = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchTen = models.CharField(max_length = 25, null = True, blank = True)
+	answerTen = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchEleven = models.CharField(max_length = 25, null = True, blank = True)
+	answerEleven = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchTwelve = models.CharField(max_length = 25, null = True, blank = True)
+	answerTwelve = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchThirteen = models.CharField(max_length = 25, null = True, blank = True)
+	answerThirteen = models.CharField(max_length = 25, null = True, blank = True)
+
+	matchFourteen = models.CharField(max_length = 25, null = True, blank = True)
+	answerFourteen = models.CharField(max_length = 25, null = True, blank = True)
+	
+	matchFifteen = models.CharField(max_length = 25, null = True, blank = True)
+	answerFifteen5 = models.CharField(max_length = 25, null = True, blank = True)
+	
+	
+	
+	
